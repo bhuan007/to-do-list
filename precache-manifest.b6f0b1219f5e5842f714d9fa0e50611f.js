a650c7c4f6187c880d2f67a15700a8d4da1267a9
Bryan Huang <bryanleehuang1@gmail.com>
2020-02-04 20:38:53 -0800

    Updates

---
+++ precache-manifest.b6f0b1219f5e5842f714d9fa0e50611f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9b75191a89adefc7759908a69c70714",
+    "url": "/to-do-list/index.html"
+  },
+  {
+    "revision": "e0577f23be0d54e4c5f2",
+    "url": "/to-do-list/static/css/main.5fefa6b5.chunk.css"
+  },
+  {
+    "revision": "39a33c1ee452a5ee85c0",
+    "url": "/to-do-list/static/js/2.6024774e.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/to-do-list/static/js/2.6024774e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e0577f23be0d54e4c5f2",
+    "url": "/to-do-list/static/js/main.8c6ee5dc.chunk.js"
+  },
+  {
+    "revision": "a3434e8c83345fbf9267",
+    "url": "/to-do-list/static/js/runtime-main.da4301b2.js"
+  }
+]);
